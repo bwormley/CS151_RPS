04fa7e4b895de33d1b087ac004323eb500e5e9ee
Brett Wormley <brett.wormley@gmail.com>
2012-03-02 15:35:59 -0800

    initial, c/o Raman

---
+++ src/cs151_rps/Referee.java
@@ -0,0 +1,109 @@
+/*
+ * Group: Lunar
+ * Members: Raman Kaur, Brett Wormley, Swathi Kotturu, Akashaya Singh
+ * Rock Paper Scissors 
+ */
+package cs151_rps;
+
+public class Referee { 
+    
+    public enum Winner {PLAYER1, PLAYER2, TIE}
+    
+    Scorecard thescoreCard = new Scorecard(); 
+    
+    public Winner determineWinner (GameObject playerOne, GameObject playerTwo){
+        if (playerOne.equals(GameObject.ROCK) && playerTwo.equals(GameObject.ROCK)) {
+            updateScore(Winner.TIE); 
+            return Winner.TIE; 
+        }
+        else if (playerOne.equals(GameObject.ROCK) && playerTwo.equals(GameObject.PAPER)) {
+            updateScore(Winner.PLAYER2); 
+            return Winner.PLAYER2;
+        }
+        else if (playerOne.equals(GameObject.ROCK) && playerTwo.equals(GameObject.SCISSORS)) {
+            updateScore(Winner.PLAYER1); 
+            return Winner.PLAYER1;
+        }
+        else if (playerOne.equals(GameObject.PAPER) && playerTwo.equals(GameObject.ROCK)) {
+            updateScore(Winner.PLAYER1); 
+            return Winner.PLAYER1;
+        }
+        else if (playerOne.equals(GameObject.PAPER) && playerTwo.equals(GameObject.PAPER)) {
+            updateScore(Winner.TIE); 
+            return Winner.TIE;
+        }
+        else if (playerOne.equals(GameObject.PAPER) && playerTwo.equals(GameObject.SCISSORS)) {
+            updateScore(Winner.PLAYER2); 
+            return Winner.PLAYER2;
+        }
+        else if (playerOne.equals(GameObject.SCISSORS) && playerTwo.equals(GameObject.ROCK)) {
+            updateScore(Winner.PLAYER2); 
+            return Winner.PLAYER2;
+        }
+        else if (playerOne.equals(GameObject.SCISSORS) && playerTwo.equals(GameObject.PAPER)) {
+            updateScore(Winner.PLAYER1); 
+            return Winner.PLAYER1;
+        }
+        else //if playerOne.equals(GameObject.SCISSORS) && playerTwo.equals(GameObject.SCISSORS) 
+        {
+            updateScore(Winner.TIE); 
+            return Winner.TIE;
+        }
+        
+    }
+   /* public Winner determineWinner (GameObject playerOne, GameObject playerTwo) {
+        if (playerOne.equals(GameObject.ROCK)) {
+            switch(playerTwo) {
+                case ROCK: 
+                    updateScore(Winner.TIE); 
+                    return Winner.TIE; 
+                case PAPER: 
+                    updateScore(Winner.PLAYER2); 
+                    return Winner.PLAYER2; 
+                case SCISSORS:
+                    updateScore(Winner.PLAYER1); 
+                    return Winner.PLAYER1; 
+            } //end switch
+        } //end if 
+        else if (playerOne.equals(GameObject.PAPER)) {
+            switch(playerTwo) {
+                case ROCK: 
+                    updateScore(Winner.PLAYER1); 
+                    return Winner.PLAYER1; 
+                case PAPER: 
+                    updateScore(Winner.TIE); 
+                    return Winner.TIE; 
+                case SCISSORS:
+                    updateScore(Winner.PLAYER2); 
+                    return Winner.PLAYER2; 
+            } //end switch 
+        } //end else if 
+        else /*playerOne is SCISSORS*/ /*{
+            switch(playerTwo) {
+                case ROCK: 
+                    updateScore(Winner.PLAYER2); 
+                    return Winner.PLAYER2; 
+                case PAPER: 
+                    updateScore(Winner.PLAYER1); 
+                    return Winner.PLAYER1; 
+                case SCISSORS:
+                    updateScore(Winner.TIE); 
+                    return Winner.TIE; 
+            }
+        } //end else 
+        return null; 
+    } //end method determine winner 
+*/
+    
+    public void updateScore(Winner winner) {
+        switch(winner) {
+            case PLAYER1: 
+                thescoreCard.setPlayerOneScore(thescoreCard.getPlayerOneScore() + 1);
+            case PLAYER2:
+                thescoreCard.setPlayerTwoScore(thescoreCard.getPlayerTwoScore()+1);
+            case TIE:
+                thescoreCard.setNumofTies(thescoreCard.getNumofTies() + 1);
+        }
+    }
+}
+
---
+++ src/cs151_rps/Scorecard.java
@@ -0,0 +1,36 @@
+/*
+ * Group: Lunar
+ * Members: Raman Kaur, Brett Wormley, Swathi Kotturu, Akashaya Singh
+ * Rock Paper Scissors 
+ */
+package cs151_rps;
+
+public class Scorecard {
+    private int playerOneScore = 0; 
+    private int playerTwoSocre = 0; 
+    private int numofTies = 0; 
+    
+    public void setPlayerOneScore (int userScore) {
+        playerOneScore = userScore; 
+    }
+    
+    public void setPlayerTwoScore(int computerScore) {
+        playerTwoSocre = computerScore; 
+    }
+    
+    public void setNumofTies(int ties) {
+        numofTies = ties; 
+    }
+    
+    public int getPlayerOneScore() {
+        return playerOneScore; 
+    }
+    
+    public int getPlayerTwoScore () {
+        return playerTwoSocre; 
+    }
+    
+    public int getNumofTies() {
+        return numofTies; 
+    }
+}
