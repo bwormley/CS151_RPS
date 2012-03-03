234c2aac0db277c2d0daffdad4cf026242aa5405
Brett Wormley <brett.wormley@gmail.com>
2012-03-02 21:08:32 -0800

    bugfix: switch cases need breaks\!

--- src/cs151_rps/Referee.java
+++ src/cs151_rps/Referee.java
@@ -56,58 +56,15 @@ public class Referee {
         }
         
     }
-   /* public Winner determineWinner (GameObject playerOne, GameObject playerTwo) {
-        if (playerOne.equals(GameObject.ROCK)) {
-            switch(playerTwo) {
-                case ROCK: 
-                    updateScore(Winner.TIE); 
-                    return Winner.TIE; 
-                case PAPER: 
-                    updateScore(Winner.PLAYER2); 
-                    return Winner.PLAYER2; 
-                case SCISSORS:
-                    updateScore(Winner.PLAYER1); 
-                    return Winner.PLAYER1; 
-            } //end switch
-        } //end if 
-        else if (playerOne.equals(GameObject.PAPER)) {
-            switch(playerTwo) {
-                case ROCK: 
-                    updateScore(Winner.PLAYER1); 
-                    return Winner.PLAYER1; 
-                case PAPER: 
-                    updateScore(Winner.TIE); 
-                    return Winner.TIE; 
-                case SCISSORS:
-                    updateScore(Winner.PLAYER2); 
-                    return Winner.PLAYER2; 
-            } //end switch 
-        } //end else if 
-        else /*playerOne is SCISSORS*/ /*{
-            switch(playerTwo) {
-                case ROCK: 
-                    updateScore(Winner.PLAYER2); 
-                    return Winner.PLAYER2; 
-                case PAPER: 
-                    updateScore(Winner.PLAYER1); 
-                    return Winner.PLAYER1; 
-                case SCISSORS:
-                    updateScore(Winner.TIE); 
-                    return Winner.TIE; 
-            }
-        } //end else 
-        return null; 
-    } //end method determine winner 
-*/
     
     public void updateScore(Winner winner) {
         switch(winner) {
             case PLAYER1: 
-                thescoreCard.setPlayerOneScore(thescoreCard.getPlayerOneScore() + 1);
+                thescoreCard.setPlayerOneScore(thescoreCard.getPlayerOneScore() + 1); break;
             case PLAYER2:
-                thescoreCard.setPlayerTwoScore(thescoreCard.getPlayerTwoScore()+1);
+                thescoreCard.setPlayerTwoScore(thescoreCard.getPlayerTwoScore()+1); break;
             case TIE:
-                thescoreCard.setNumOfTies(thescoreCard.getNumOfTies() + 1);
+                thescoreCard.setNumOfTies(thescoreCard.getNumOfTies() + 1); break;
         }
     }
 }
