9cbc2418cac87707c9f42899f5a21126e451228b
Swathi Kotturu <kotturuswathi@yahoo.com>
2012-03-19 18:12:26 -0700

    modified, so throws move that beats predicted move instead of actual move. also added randomThrowGen

--- src/cs151_rps/play/SmartThrowGenerator.java
+++ src/cs151_rps/play/SmartThrowGenerator.java
@@ -6,62 +6,74 @@ public class SmartThrowGenerator extends ThrowGenerator
 {
 	private Analysis analysis;
 	private History hist;
+	private RandomThrowGenerator rand;
+	private int n;
 	
 	public SmartThrowGenerator()
 	{
 		analysis = Analysis.factory("shortTerm");
 		hist = History.factory("shortTerm");
+		rand = new RandomThrowGenerator();
+		//default
+		n = 3;
+	}
+	
+	public setN(int x)
+	{
+		n = x;
 	}
 	
 	/**
-     * Finds out a computer's choice by looking back at the history.
+     * Finds out a computer's choice by looking back at the history. 
+     * This player's moves are upper case and other player's moves are lower case.
      */
     @Override
 	public GameObject queryThrow() throws Exception
 	{
-    	if(analysis.numberOfMovesSoFar <= 3)
-    	{
-    		//do whatever fits
-    		return null;//TODO change
-    	}
-    	else
-    	{
-		int nMoves = 3;
-		String lastNMoves = hist.getLastMoves(3);
-		String Rnext = lastNMoves + "r";
-		String Pnext = lastNMoves + "p";
-		String Snext = lastNMoves + "s";
-		
-		int freqOfRNext = analysis.getFreq(Rnext);
-		int freqOfPNext = analysis.getFreq(Pnext);
-		int freqOfSNext = analysis.getFreq(Snext);
-		
-		
-		/**
-		 * Covers:
-		 *  R > P > S
-			R > S > P
-			S > R > P
-			S > P > R
-			P > R > S
-			P > S > R
-		 */
-		
-		if(freqOfRNext >= freqOfPNext && freqOfRNext >= freqOfSNext)
+		if (analysis.numberOfMovesSoFar <= n) 
 		{
-			return GameObject.ROCK;
-		}
-		else if(freqOfSNext >= freqOfPNext && freqOfSNext >= freqOfRNext)
-		{
-			return GameObject.SCISSORS;
+			return rand.queryThrow();
 		}
 		else 
 		{
-			return GameObject.PAPER;
+			//last strings
+			String pastNMoves = analysis.pastNMoves(n);
+			//possible other player's moves
+			String Rnext = lastNMoves + "r";
+			String Pnext = lastNMoves + "p";
+			String Snext = lastNMoves + "s";
+			//freq of possible moves
+			int freqOfRNext = analysis.getFreq(Rnext);
+			int freqOfPNext = analysis.getFreq(Pnext);
+			int freqOfSNext = analysis.getFreq(Snext);
+
+			/**
+			 * Covers: 
+			 * R > P > S 
+			 * R > S > P 
+			 * S > R > P 
+			 * S > P > R 
+			 * P > R > S 
+			 * P > S > R
+			 */
+
+			if (freqOfRNext >= freqOfPNext && freqOfRNext >= freqOfSNext) 
+			{
+				//if rock is most likely to be played -> return paper
+				return GameObject.PAPER;
+			} 
+			else if (freqOfSNext >= freqOfPNext && freqOfSNext >= freqOfRNext) 
+			{
+				//if scissors is most likely to be played -> return rock
+				return GameObject.ROCK;
+			} 
+			else
+			{
+				//if paper is most likely to be played -> return scissors
+				return GameObject.SCISSORS;
+			}
+
 		}
-    	
-    	}
-		
-    	
-	}
-}
+
+	}// end of QueryThrow
+}// end of SmartThrowGenerator
