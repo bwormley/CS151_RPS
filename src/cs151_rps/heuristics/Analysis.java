5df673d9b7124ee6e7a7daefbd723c88265e2682
Brett Wormley <brett.wormley@gmail.com>
2012-03-20 22:55:59 -0700

    updated Analysis.java to reflect discussion of the contract with History and SmartThrowGenerator

--- src/cs151_rps/heuristics/Analysis.java
+++ src/cs151_rps/heuristics/Analysis.java
@@ -9,20 +9,77 @@ package cs151_rps.heuristics;
  * the N level. And based on that N level, the History class will pass N length 
  * of string. */
 
-public interface Analysis {
+abstract public class Analysis {
     
-    //This history class will pass the the N moves to add in hashmap
-    public String getMoves(String moves); 
+    /**
+     * Constructor
+     */
+    public Analysis( String type )
+    {
+        // retrieve the pre-existing History singleton instance
+        // (note: instance already created by Referee class)
+        history = History.factory("");
+    }
     
+    /**
+     * Implementation of a history persistence model
+     */
+    protected History history;
+    
+    /**
+     * The analysis is to happen for patterns between these 
+     * two lengths, inclusive
+     */
+    private final int MIN_DEPTH = 2;
+    private final int MAX_DEPTH = 5;
+    
+    /**
+     * Create an instance of a subclass to implement a game heuristic.
+     * 
+     * @param type - level of the heuristic algorithm
+     * @return object implementing the heuristic algorithm
+     */
+    public static Analysis factory( String type )
+    {
+        return null;
+    }
+    
+    /**
+     * Update the ongoing analysis with the results of a new round.  This 
+     * method is called once (and only once!) per round.  The History class 
+     * will have a record of the last round's results.
+     */
+    public abstract void update();
+    
+    /**
+     * Return the last gestures  made in the game
+     * 
+     * @param numberOfMoves number of moves made, 
+     * or null if insufficient history
+     */
+    public String getLastMoves( int numberOfMoves )
+    {
+        return history.getLastMoves(numberOfMoves);
+    }
+    
+    /**
+     * Return the number of times this pattern of gestures has occurred
+     * 
+     * @param pattern a string of r/p/s characters indicating a pattern of 
+     * gestures
+     * @return a non-negative number of times the pattern appeared
+     */
+    abstract int frequencyOf( String pattern );
+            
     //If the String moves already exist in the map, the value will be incremented
     //by one. Else the moves will be added and the value will be one. 
-    public void addMoves(String moves); 
+    //abstract protected void addMoves(String moves); 
     
     //This method will give the Smart Throw Generator class what the predicsted 
     //next move is. 
-    public String predictedMove(int N); 
+    //public String predictedMove(int N); 
     
     //This method will will fetch the recent N moves from the History class to 
     //predict the next move. 
-    public String pastNMoves(int N); 
+    //public String pastNMoves(int N); 
 }
