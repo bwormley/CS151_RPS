af31429791c18869ca49e9a20ff118a42258cc34
Ramanprit <ramanprit9@yahoo.com>
2012-03-26 13:12:04 -0700

    Raman - created ShortTermAnalysisTest class - all methods working

---
+++ test/cs151_rps/heuristics/ShortTermAnalysisTest.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cs151_rps.heuristics;
+
+import cs151_rps.GameObject;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sohi_x2
+ */
+public class ShortTermAnalysisTest {
+    
+    private ShortTermAnalysis st; 
+    public ShortTermAnalysisTest() {
+        st = new ShortTermAnalysis(""); 
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class ShortTermAnalysis.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        st.add("RPS");
+        st.add("rps"); 
+        st.add("pps"); 
+        st.add("ssrrr"); 
+        int frequency = st.frequencyOf("rps"); 
+        assertTrue(frequency == 2); 
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of update method, of class ShortTermAnalysis.
+     */
+    @Test
+    public void testUpdate() {
+        System.out.println("update");
+        ShortTermHistory history = new ShortTermHistory(); 
+        history.addRound(GameObject.PAPER, GameObject.PAPER);
+        history.addRound(GameObject.ROCK, GameObject.SCISSORS); 
+        history.addRound(GameObject.PAPER, GameObject.SCISSORS); 
+        int i; 
+        //add the last moves in the hashmap using the body for the update method
+        for (i = st.MAX_DEPTH; i >= st.MIN_DEPTH; i--) {
+            st.add(history.getLastMoves(i)); 
+        }
+        
+        history.addRound(GameObject.PAPER, GameObject.SCISSORS);
+        
+        //add the last moves in the hashmap using the body for the update method
+        for (i = st.MAX_DEPTH; i >= st.MIN_DEPTH; i--) {
+            st.add(history.getLastMoves(i)); 
+        }
+        int frequency = st.frequencyOf("sps"); 
+        int expFrequency = 2; 
+        //assertEquals(expFrequency, frequency);
+        assertEquals(frequency, expFrequency); 
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of frequencyOf method, of class ShortTermAnalysis.
+     */
+    @Test
+    public void testFrequencyOf() {
+        System.out.println("frequencyOf");
+        int expResult = 1;
+        st.add("rrr"); 
+        int result = st.frequencyOf("rrr");
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        //fail("The test case is a prototype.");
+    }
+}
