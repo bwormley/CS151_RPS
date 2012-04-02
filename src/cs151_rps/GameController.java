3a381e6c29f8fc56ab4495c5124e4ca352733773
Brett Wormley <brett.wormley@gmail.com>
2012-04-02 09:08:31 -0700

    cleaned up comments and coding conventions in the main class

--- src/cs151_rps/GameController.java
+++ src/cs151_rps/GameController.java
@@ -1,23 +1,55 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package cs151_rps;
 
+import static cs151_rps.GameObject.ROCK;
+import static cs151_rps.ParseCL.*;
+import cs151_rps.io.UIO;
 import cs151_rps.player.ComputerPlayer;
 import cs151_rps.player.Player;
 import cs151_rps.score.Referee;
+import cs151_rps.score.Referee.Winner;
+import static cs151_rps.score.Referee.Winner.PLAYER1;
+import static cs151_rps.score.Referee.Winner.PLAYER2;
 import cs151_rps.score.Scorecard;
 
-import static cs151_rps.ParseCL.*;
-import cs151_rps.io.UIO;
-
 /**
- * The top-level class that coordinates interaction among the actors and the underlying system.
+ * ROCK-PAPER-SCISSORS: The top-level class that coordinates interaction 
+ * among the actors and the underlying system.
  * 
- * @author bwormley
+ * @author Team Lunar
  */
 public class GameController {
+    
+    /**
+     * Object encapsulating command line parser, and the command line 
+     * argument values, and defaults
+     */
+    private ParseCL args;
+
+    /**
+     * Implements the rules of the game, updates scorecard for each round, 
+     * and forwards player moves to be archived
+     */
+    private Referee referee;
+
+    /**
+     * Maintains the current score for the match, and aggregates the 
+     * persistence model required by any heuristic algorithm
+     */
+    private Scorecard scorecard;
+    
+    /**
+     * The two actors (players) in the game.  From the perspective of this 
+     * class, these types are generic, encapsulating whether or not they are 
+     * human/AI, what i/o method they use, what heuristic they use, or even 
+     * if the player is local or remote.
+     */
+    private Player player1;
+    private Player player2;
+    
+    /**
+     * Indication of experience level for the heuristics of the AI player
+     */
+    private String level;
     
     /**
     * Constructor - create a game, and initialize with all parameters 
@@ -30,115 +62,86 @@ public class GameController {
     {
         this.args = args;
     }
-    
-    ParseCL args;
 
-    
-    //private ShortTermHistory history; 
-    
     /**
-     * Implements the rules of the game, updates scorecard for each round, 
-     * and forwards player moves to be archived
-     */
-    private Referee referee;
-    
-    //updates the map in the short term 
-    /**
-     * Maintains the current score for the match
-     */
-    private Scorecard scorecard;
-    
-    /**
-     * The number of rounds to play in this match
-     */
-    private int maxRounds;
-    
-    /**
-     * Indication of experience level for computer player
-     */
-    private String level;
-    
-    /**
-     * The two ctors in the game
-     */
-    private Player player1;
-    private Player player2;
-    
-    /**
-     * The actors' throws for a single round
-     */
-    private GameObject player1Throw;
-    private GameObject player2Throw;
-    
-    /**
-     * Endpoint for all program output (e.g., CLI, Swing, Remote)
+     * Endpoint for all program input and output (e.g., CLI, Swing, Remote)
      */
     UIO endpoint;
     
     /**
      * Create a Player object based on runtime attributes from the command line
      * 
-     * @param endpoint - object abstracting all input
+     * @param endpoint - object abstracting all input and output
      * @param playerType - human, AI, etc
-     * @param inputType - CLI, Swing, etc
-     * @param level - smart, etc
-     * @return provisioned player object of the correct identity
+     * @param level - random, smart, etc
+     * @return player object of the correct identity, 
+     * provisioned with an io endpoint
      */
-    Player provisionPlayer( 
-            UIO endpoint, 
-            String playerType, 
-            String inputType, 
-            String level  )
+    private Player provisionPlayer( 
+                   UIO endpoint, 
+                   String playerType, 
+                   String level  )
     {
         String name = "";
+        
+        // if a human player, ask for his name
         if (playerType.equalsIgnoreCase("human"))
         {
             endpoint.displayWelcome();
             endpoint.displayNamePrompt();
             name = endpoint.getPlayerName();
             if(name==null || name.isEmpty())
-            {
-            	name = "PLAYER";
-            }
+            	name = "HUMAN";
         }
+        
+        // create a player with the correct identity
         Player player = Player.factory( playerType, name, endpoint );
+        
+        // if an AI player, set the heuristic level
         if (player instanceof ComputerPlayer)
             ((ComputerPlayer)player).setExperienceLevel(level);
-       return player;
+        
+        return player;
     }
 
     /**
-     * Main controlling method for the game
+     * MAIN GAME CONTROLLER METHOD
      */
     public void run()
     {
-        // set up selected input and output endpoints
-        endpoint = UIO.factory( args.getStringParam(IO_OPTION), args.getStringParam(LANG_OPTION) );
+        // the actors' throws for a single round
+        GameObject player1Throw = ROCK;
+        GameObject player2Throw = ROCK;
+    
+        // the number of rounds to play in this match
+        int maxRounds;
+    
+        // set up the input and output endpoint, set from the command line
+        endpoint = UIO.factory( args.getStringParam(IO_OPTION), 
+                                args.getStringParam(LANG_OPTION) );
         
-        // flexibly provision two players
+        // flexibly provision the two players
         player1 = provisionPlayer( 
                 endpoint, 
                 args.getStringParam(PLAYER_ONE_OPTION),
-                args.getStringParam(IO_OPTION),
                 args.getStringParam(EXPERIENCE_OPTION) );
         player2 = provisionPlayer( 
                 endpoint, 
                 args.getStringParam(PLAYER_TWO_OPTION),
-                args.getStringParam(IO_OPTION) ,
                 args.getStringParam(EXPERIENCE_OPTION) );
 
-        // instantiate system objects
-        // Design Pattern: Dependency Injection (Fowler, Type 3)
+        // instantiate the system objects
         scorecard = new Scorecard();
-        referee = new Referee( scorecard, args.getStringParam(EXPERIENCE_OPTION) );
+        referee = new Referee( scorecard, 
+                               args.getStringParam(EXPERIENCE_OPTION) );
         
         // initialize any final runtime parameters
         maxRounds = args.getIntParam(NUM_ROUNDS_OPTION);
         
-        // main game loop
+        // MAIN GAME LOOP
         for ( int round=1; round <= maxRounds; round++ ) {
 
-            // start new round; annouonce round number
+            // start new round; announce the round number
             endpoint.displayNewRound();
             endpoint.displayRound( round, maxRounds );
             
@@ -151,56 +154,46 @@ public class GameController {
                 endpoint.displayThrow( player2.getName(), player2Throw );
             }
             catch (Exception e) {
-            	if (e.getMessage() != null && e.getMessage().equalsIgnoreCase("score"))
-            	{
-            		score = true;
-            		round--;
+            	if (e.getMessage() != null 
+                        && e.getMessage().equalsIgnoreCase("score")) {
+                    score = true;
+                    round--;
             	}
-            	if(e.getMessage() != null && e.getMessage().equalsIgnoreCase("user abort"))
-            	{
-            		endpoint.displayQuit();
-                System.exit(0);
-                // fatal error: abort here, with specific cosolation.
+            	if(e.getMessage() != null 
+                        && e.getMessage().equalsIgnoreCase("user abort")) {
+                    endpoint.displayQuit();
+                    System.exit(0);
+                    // fatal error: abort here, with specific cosolation.
             	}
-   
-            		
             }
 
-            // determine the winner, and display it
-            if(score == false)
-            {
-            Referee.Winner winner = referee.determineWinner( player1Throw, player2Throw );
-            if (winner==Referee.Winner.PLAYER1)
-                endpoint.displayRoundWinner( player1.getName() );
-            else if (winner==Referee.Winner.PLAYER2)
-                endpoint.displayRoundWinner( player2.getName() );
-            else 
-                endpoint.displayRoundTie( player1.getName(), player2.getName() );
+            // determine the round's winner, and display him
+            if(score == false) {
+                Winner winner = referee.determineWinner( player1Throw, 
+                                                         player2Throw );
+                if (winner==PLAYER1)
+                    endpoint.displayRoundWinner( player1.getName() );
+                else if (winner==PLAYER2)
+                    endpoint.displayRoundWinner( player2.getName() );
+                else 
+                    endpoint.displayRoundTie( player1.getName(), 
+                                              player2.getName() );
             }
      
             // display match score so far
-            endpoint.displayScore( player1.getName(), scorecard.getPlayerOneScore(),
-                                   player2.getName(), scorecard.getPlayerTwoScore(),
-                                                      scorecard.getNumOfTies() );
-            
+            endpoint.displayScore( 
+                    player1.getName(), scorecard.getPlayerOneScore(),
+                    player2.getName(), scorecard.getPlayerTwoScore(),
+                                       scorecard.getNumOfTies() );
         }
         
-        // display winner of the match
-        
+        // display the winner of the match
         if(scorecard.getPlayerOneScore() > scorecard.getPlayerTwoScore())
-        {
             endpoint.displayMatchWinner(player1.getName());
-        }
         else if (scorecard.getPlayerOneScore() < scorecard.getPlayerTwoScore())
-        {
             endpoint.displayMatchWinner(player2.getName());
-        }
         else 
-        {
-            endpoint.displayMatchIsTie();
-        }
-        
-        
+            endpoint.displayMatchIsTie();        
     }
-    
+
 }
